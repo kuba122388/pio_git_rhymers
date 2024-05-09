4f3263afc2ce6a89fed7a86222aa954f90b7f822
Michal Strozak <245931@edu.p.lodz.pl>
2024-05-09 14:54:47 +0200

    11. Hermetyzacja nieprywatnch atrybutów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int num12 = 12;
-    private static final int num1m = -1;
-    private static final int num11 = 11;
-    private final int[] numbers = new int[num12];
+    private static int num12 = 12;
+    private static int num1m = -1;
+    private static int num11 = 11;
+    private int[] numbers = new int[num12];
 
     private int total = num1m;
+
+    public static int getNum12() {
+        return num12;
+    }
+
+    public static void setNum12(int num12) {
+        DefaultCountingOutRhymer.num12 = num12;
+    }
+
+    public static int getNum1m() {
+        return num1m;
+    }
+
+    public static void setNum1m(int num1m) {
+        DefaultCountingOutRhymer.num1m = num1m;
+    }
+
+    public static int getNum11() {
+        return num11;
+    }
+
+    public static void setNum11(int num11) {
+        DefaultCountingOutRhymer.num11 = num11;
+    }
 
     public int getTotal() {
         return total;
@@ -39,4 +63,15 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
