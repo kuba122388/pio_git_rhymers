58385b45b88cd767e27b9f186999176fcad89710
Michal Strozak <245931@edu.p.lodz.pl>
2024-05-09 14:58:58 +0200

    12. Usunięcie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -13,24 +13,12 @@ public class DefaultCountingOutRhymer {
         return num12;
     }
 
-    public static void setNum12(int num12) {
-        DefaultCountingOutRhymer.num12 = num12;
-    }
-
     public static int getNum1m() {
         return num1m;
     }
 
-    public static void setNum1m(int num1m) {
-        DefaultCountingOutRhymer.num1m = num1m;
-    }
-
     public static int getNum11() {
         return num11;
-    }
-
-    public static void setNum11(int num11) {
-        DefaultCountingOutRhymer.num11 = num11;
     }
 
     public int getTotal() {
@@ -65,13 +53,5 @@ public class DefaultCountingOutRhymer {
 
     public int[] getNumbers() {
         return numbers;
-    }
-
-    public void setNumbers(int[] numbers) {
-        this.numbers = numbers;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
     }
 }
